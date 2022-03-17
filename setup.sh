758c8bd33169c49884a16a367d2fff0560fd02e3
Zorin <zorin1477@gmail.com>
2022-03-17 16:29:55 +0800

    fix: simplify code

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 alwaysRun=false
 video=''
-needChmod=false
 while getopts ":ap:h" arg
 do
     case "$arg" in
@@ -12,68 +11,62 @@ do
         video=$OPTARG
         ;;
       "h")
-        cat << EOF
+        cat << EOL
 Options:
         -a: Always run video wallpaper.
         -p: Path to video.
         -h: Display this text.
 
-EOF
+EOL
         ;;
       ":")
         echo "ERROR: No argument value for option $OPTARG"
+        exit
         ;;
       "?")
         echo "ERROR: Unknown option $OPTARG"
+        exit
         ;;
       "*")
         echo "ERROR: Unknown error while processing options"
+        exit
         ;;
     esac
 done
-generateScript() {
-    if [ $alwaysRun == true ]; then
-        cat > start.sh << EOF
-#!/bin/bash
-## kill and start video background
-killall xwinwrap
-while pgrep -u $UID -x xwinwrap >/dev/null; do sleep 1; done
-xwinwrap -fs -fdt -ni -b -nf -ov -- mpv --input-ipc-server=/tmp/mpvsocket --no-audio --loop -wid WID ${video} &
-EOF
-        echo "INFO: Script was generated."
-    else
-        cat > start.sh << EOF
-#!/bin/bash
-## kill and start video background
-killall xwinwrap
-while pgrep -u $UID -x xwinwrap >/dev/null; do sleep 1; done
-xwinwrap -fs -fdt -ni -b -nf -ov -- mpv --input-ipc-server=/tmp/mpvsocket --no-audio --pause --loop -wid WID ${video} &
-PLAY=false
-while true;
-do 
-    if [ "\$(xdotool getwindowfocus getwindowname)" == "i3" ] && [ \$PLAY == false ]; then
-            echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
-            PLAY=true
-    elif [ "\$(xdotool getwindowfocus getwindowname)" != "i3" ] && [ \$PLAY == true ]; then
-            echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
-            PLAY=false
-    fi
-    sleep 1
-done
-EOF
-        echo "INFO: Script was generated."
-    fi
-    if [ $needChmod == true ]; then
-        chmod +x start.sh
-    fi
-    echo "Info: Completed."
+
+run_always() {
+  ## kill and start video background
+  killall xwinwrap
+  sleep 1
+  xwinwrap -fs -fdt -ni -b -nf -ov -- mpv --input-ipc-server=/tmp/mpvsocket --no-audio --loop -wid WID "$video" &
 }
-if [ ! $video ]; then
-    echo "ERROR: The video path is empty."
-elif [ ! -f "start.sh" ]; then
-    echo "INFO: It seems that the file start.sh does not exist."
-    needChmod=true
-    generateScript
+
+run() {
+  PLAY=false
+  # ## kill and start video background
+  killall xwinwrap
+  sleep 1
+  xwinwrap -fs -fdt -ni -b -nf -ov -- mpv --input-ipc-server=/tmp/mpvsocket --no-audio --pause --loop -wid WID "$video" &
+  while true;
+  do
+      if [ "$(xdotool getwindowfocus getwindowname)" == "i3" ] && [ $PLAY == false ]; then
+              echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
+              PLAY=true
+      elif [ "$(xdotool getwindowfocus getwindowname)" != "i3" ] && [ $PLAY == true ]; then
+              echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
+              PLAY=false
+      fi
+      sleep 1
+  done
+}
+
+if [ ! -f "$video" ]; then
+  echo "ERROR: The video path is empty."
+  exit
+fi
+
+if [ $alwaysRun == true ]; then
+  run_always
 else
-    generateScript
+  run
 fi

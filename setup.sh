58682293a8aacc6c2c51cb69c89b5dbea584ec9d
Zolyn <zorin1477@gmail.com>
2022-07-07 05:28:03 +0800

    feat!: support multiple videos && refactor options

--- setup.sh
+++ setup.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 alwaysRun=false
-video=''
 generateThumbnail=false
 setWallpaper=false
 blur=false
-blurGeometry='16x16'
-fit="fill"
+fit='fill'
 thumbnailStorePath="$HOME/Pictures/i3-video-wallpaper"
-thumbnailPath=""
-timeStamp='00:00:01'
 
 PIDFILE="/var/run/user/$UID/vwp.pid"
-monitor_index=0
 
 declare -a PIDs
-declare -a MONITORs
+declare -a Monitors
+declare -a thumbnailList
 
-while getopts ":ap:nwbg:f:d:t:h" arg
+while getopts ":anwbf:d:h" arg
 do
     case "$arg" in
       "a")
         alwaysRun=true
-        ;;
-      "p")
-        video=$OPTARG
         ;;
       "n")
         generateThumbnail=true
@@ -34,36 +27,35 @@ do
       "b")
         blur=true
         ;;
-      "g")
-        blurGeometry=$OPTARG
-        ;;
       "f")
         fit=$OPTARG
         ;;
       "d")
         thumbnailStorePath=$OPTARG
         ;;
-      "t")
-        timeStamp=$OPTARG
-        ;;
       "h")
         cat << EOL
+Usage:
+./setup.sh [OPTIONS] [VIDEO,BLUR_GEOMETRY,TIME_STAMP]
+
+Example:
+./setup.sh -a -n -w -b video.mp4 video2.mp4,32x32
+
+NOTE: The path of the video(s) must be the last parameter!
+
 Options:
         -a: Always run video wallpaper.
-        -p: Path to video.
         -n: Generate a thumbnail by using ffmpeg. It may be useful if you use the built-in system tray of Polybar. (This can fix the background of system tray)
         -w: Set the generated thumbnail as wallpaper by using feh. It may be useful if you use the built-in system tray of Polybar. (This can fix the background of system tray)
         -b: Blur the thumbnail. It may be useful if your compositor does not blur the background of the built-in system tray of Polybar.
-        -g: Parameter which is passed to "convert -blur [parameter]". (Default: 16x16)
         -f: Parameter which is passed to "feh --bg-[paramater]". Available options: center|fill|max|scale|tile (Default: fill)
         -d: Where the thumbnails is stored. (Default: $HOME/Pictures/i3-video-wallpaper)
-        -t: The time to generate the thumbnail. (Default: 00:00:01) 
         -h: Display this text.
 
 EOL
         ;;
       ":")
-        echo "ERROR: No argument value for option $OPTARG"
+        echo "ERROR: Option $OPTARG requires argument(s)"
         exit
         ;;
       "?")
@@ -77,6 +69,9 @@ EOL
     esac
 done
 
+# Remove options
+shift $((OPTIND - 1))
+
 kill_xwinwrap() {
   while read p; do
     [[ $(ps -p "$p" -o comm=) == "xwinwrap" ]] && kill -9 "$p";
@@ -86,21 +81,25 @@ kill_xwinwrap() {
 
 play() {
   if [ $alwaysRun == true ]; then
-    xwinwrap -ov -ni -g "$1" -- mpv --fs --loop-file --no-audio --no-osc --no-osd-bar -wid WID --no-input-default-bindings "$video" &
+    xwinwrap -ov -ni -g "$1" -- mpv --fs --loop-file --no-audio --no-osc --no-osd-bar -wid WID --no-input-default-bindings "$2" &
   else
     isPlaying=false
-    xwinwrap -ov -ni -g "$1" -- mpv --fs --loop-file --input-ipc-server="/tmp/mpvsocket$monitor_index" --pause --no-audio --no-osc --no-osd-bar -wid WID --no-input-default-bindings "$video" &
+    xwinwrap -ov -ni -g "$1" -- mpv --fs --loop-file --input-ipc-server="/tmp/mpvsocket$3" --pause --no-audio --no-osc --no-osd-bar -wid WID --no-input-default-bindings "$2" &
   fi
   PIDs+=($!)
 }
 
 pause_video() {
-  for m in "${MONITORs[@]}"; do
-        echo '{"command": ["cycle", "pause"]}' | socat - "/tmp/mpvsocket$m"
+  for ((i=1; i<=${#Monitors[@]}; i++)); do
+    echo '{"command": ["cycle", "pause"]}' | socat - "/tmp/mpvsocket$i"
   done
 }
 
 generate_thumbnail() {
+  video=$1
+  timeStamp=$2
+  blurGeometry=$3
+
   videoName="$(basename "$video" ".${video##*.}")"
   thumbnailPath="$thumbnailStorePath/$videoName.png"
 
@@ -115,30 +114,79 @@ generate_thumbnail() {
     convert "$thumbnailPath" -blur "$blurGeometry" "$blurredThumbnailPath"
     thumbnailPath=$blurredThumbnailPath
   fi
+
+  thumbnailList+=("$thumbnailPath")
 }
 
-if [ ! -f "$video" ]; then
-  echo "ERROR: The video path is empty."
+check_if_video_exists() {
+  if [ ! -f "$1" ]; then
+    echo "ERROR: The video path $1 is empty."
+    exit
+  fi
+}
+
+trim() {
+  echo "$1" | grep -o "[^ ]\+\( \+[^ ]\+\)*"
+}
+
+main() {
+  video=''
+  blurGeometry='16x16'
+  timeStamp='00:00:01'
+  geometry=$2
+  monitorIndex=$3
+
+  declare -a IndexMap;
+  IndexMap=(
+    [0]="video"
+    [1]="blurGeometry"
+    [2]="timeStamp"
+  )
+
+  readarray -d , -t ValueArr <<< "$1"
+
+  for ((i=0; i<${#ValueArr[@]}; i++)); do
+    ValueArr[$i]=$(trim "${ValueArr[$i]}")
+  done
+
+  for ((i=0; i<${#IndexMap[@]}; i++)); do
+    if [ -n "${ValueArr[$i]}" ]; then
+      eval "${IndexMap[$i]}=${ValueArr[$i]}"
+    fi
+  done
+
+  check_if_video_exists "$video"
+
+  play "$geometry" "$video" "$monitorIndex"
+
+  if [ $generateThumbnail == true ]; then
+    generate_thumbnail "$video" "$timeStamp" "$blurGeometry"
+  fi
+}
+
+if [ -z "$*" ]; then
+  echo "ERROR: Requires video path"
   exit
 fi
 
 kill_xwinwrap
 
 for g in $(xrandr -q | grep 'connected' | grep -oP '\d+x\d+\+\d+\+\d+'); do
-  ((monitor_index++))
-  MONITORs+=("$monitor_index")
-  play "$g"
+  Monitors+=("$g")
+  value=$(eval echo "\$${#Monitors[@]}")
+  
+  if [ -z "$value" ]; then
+    value=$(eval echo "\$${#@}")
+  fi
+
+  main "$value" "$g" "${#Monitors[@]}"
 done
 
-if [ $generateThumbnail == true ]; then
-  generate_thumbnail
-fi
+printf "%s\n" "${PIDs[@]}" > $PIDFILE
 
 if [ $setWallpaper == true ]; then
-    feh "--bg-$fit" "$thumbnailPath"
+    feh "--bg-$fit" "${thumbnailList[@]}"
 fi
-
-printf "%s\n" "${PIDs[@]}" > $PIDFILE
 
 if [ $alwaysRun != true ]; then
   while true;
